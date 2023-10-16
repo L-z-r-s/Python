028466f52174f6f503a74e55218c7d4a8118325c
Lazarus Abraham <136486840+L-z-r-s@users.noreply.github.com>
2023-10-16 17:03:07 +0530

    Add files via upload

---
+++ Palindrome.py
@@ -0,0 +1,9 @@
+# Program to check if a given string is palindrome or not
+str_1=input("Enter the string to check if Palindrome or not:")
+str_2 = str_1[::-1]
+def palindrome(str_1):
+    return str_2
+if str_2==str_1:
+    print("The string is a palindrome")
+else:
+    print("The string is not a palindrome")
